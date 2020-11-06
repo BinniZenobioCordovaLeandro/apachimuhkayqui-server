a21ae68a9fba3be0bfa5f8be64184123e4afbdbf
Juan Javier Quispe Leiva <64117583+juanjavierquispeleiva@users.noreply.github.com>
2020-11-05 21:59:48 -0500

    Examen parcial

--- models/credentialModel.js
+++ models/credentialModel.js
@@ -1,29 +1,27 @@
 'use strict'
 
-function credentialModel (sequelize, DataTypes) {
-  const credentialModel = sequelize.define(
-    'credential',
-    {
-      user_id: DataTypes.INTEGER,
-      password: {
-        type: DataTypes.BLOB,
-        get: function () {
-          return (this.getDataValue('password')).toString('utf8')
+function credentialModel(sequelize, Datatypes) {
+    const credentialModel = sequelize.define(
+        'credential', {
+            user_id: Datatypes.INTEGER,
+            password: {
+                type: Datatypes.STRING,
+                get: function() {
+                    return (this.getDataValue("password")).toString('utf8')
+                }
+            },
+            timestamp_created: Datatypes.DATE,
+        }, {
+            timestamps: false,
+            freezeTableName: false
         }
-      },
-      timestamp_created: DataTypes.DATE
-    },
-    {
-      timestamps: false,
-      freezeTableName: false
+    )
+    credentialModel.associate = (models) => {
+        credentialModel.belongsTo(models.User, {
+            foreignKey: 'user_id'
+        })
     }
-  )
-  credentialModel.associate = (models) => {
-    credentialModel.belongsTo(models.User, {
-      foreignKey: 'user_id'
-    })
-  }
-  return credentialModel
+    return credentialModel
 }
 
 module.exports = credentialModel
