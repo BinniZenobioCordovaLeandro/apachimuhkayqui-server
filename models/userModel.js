105078777624535112ddd4d7e6b3ed16a0a84162
Fanny Aquise Carbajal <64118216+fannyaquisecarbajal@users.noreply.github.com>
2020-11-05 22:03:06 -0500

    examen parcial

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,26 @@
 'use strict'
 
 function userModel (sequelize, DataTypes) {
-  const userModel = sequelize.define(
-    'user',
-    {
-      fullname: DataTypes.STRING,
-      alias: DataTypes.STRING,
-      email: DataTypes.STRING,
-      phone: DataTypes.CHAR,
-      document: DataTypes.CHAR
-    },
-    {
-      timestamps: false,
-      freezeTableName: false
+    const userModel = sequelize.define(
+        'user',
+        {
+            fullname: DataTypes.STRING,
+            alias: DataTypes.STRING,
+            email: DataTypes.STRING,
+            phone: DataTypes.CHAR,
+            document: DataTypes.CHAR
+        },
+        { 
+            timestamps: false,
+            freezeTableName: false
+        }
+    )
+    userModel.associate = (models) => {
+        userModel.belongsTo(models.Credential, {
+            foreignKey: 'user_id'
+        })         
     }
-  )
-  userModel.associate = (models) => {
-    userModel.hasMany(models.Credential, {
-      foreignKey: 'user_id'
-    })
-  }
-  return userModel
+    return userModel
 }
 
 module.exports = userModel
