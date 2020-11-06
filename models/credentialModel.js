c29fd90e288f9b6ba0ee2bd92782b1d5e6af4971
Edgar Michael Vargas Yanque <michaelvargas0301@gmail.com>
2020-11-05 21:58:45 -0500

    Examen parcial

--- models/credentialModel.js
+++ models/credentialModel.js
@@ -1,29 +1,28 @@
 'use strict'
 
-function credentialModel (sequelize, DataTypes) {
-  const credentialModel = sequelize.define(
+function credentialModel (sequelize, Datatypes) {
+    const credentialModel = sequelize.define(
     'credential',
     {
-      user_id: DataTypes.INTEGER,
-      password: {
-        type: DataTypes.BLOB,
-        get: function () {
-          return (this.getDataValue('password')).toString('utf8')
-        }
-      },
-      timestamp_created: DataTypes.DATE
+        user_id: Datatypes.INTEGER,
+        password :{
+            type:Datatypes.STRING,
+            get: function() { 
+                return (this.getDataValue("password")).toString('utf8')    
+            }
+        },
+        timestamp_created: Datatypes.DATE,
     },
-    {
-      timestamps: false,
-      freezeTableName: false
+    {        timestamps: false,
+        freezeTableName: false 
     }
-  )
-  credentialModel.associate = (models) => {
-    credentialModel.belongsTo(models.User, {
-      foreignKey: 'user_id'
-    })
-  }
-  return credentialModel
+    )
+    credentialModel.associate = (models) => {
+        credentialModel.belongsTo(models.User, { 
+          foreignKey: 'user_id'
+        })
+    }
+    return credentialModel
 }
 
 module.exports = credentialModel
--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,42 @@
 'use strict'
 
-function userModel (sequelize, DataTypes) {
-  const userModel = sequelize.define(
+function userModel (sequelize, Datatypes) {
+    const userModel = sequelize.define(
     'user',
     {
-      fullname: DataTypes.STRING,
-      alias: DataTypes.STRING,
-      email: DataTypes.STRING,
-      phone: DataTypes.CHAR,
-      document: DataTypes.CHAR
+        fullname: Datatypes.STRING,
+        alias: Datatypes.STRING,
+        email: Datatypes.STRING,
+        phone: Datatypes.CHAR,
+        document: Datatypes.CHAR,
     },
     {
-      timestamps: false,
-      freezeTableName: false
+        timestamps: false,
+        freezeTableName: false 
     }
-  )
-  userModel.associate = (models) => {
-    userModel.hasMany(models.Credential, {
-      foreignKey: 'user_id'
-    })
-  }
-  return userModel
+    )
+    userModel.associate = (models) => {
+      userModel.hasMany(models.Credential, {
+          foreignKey: 'user_id'
+      })
+      userModel.hasMany(models.Card, {
+        foreignKey: 'user_id'
+      })
+      userModel.hasMany(models.Item, {
+          foreignKey: 'user_id'
+      })
+      userModel.hasMany(models.Store, {
+        foreignKey: 'user_id'
+      })
+      userModel.hasMany(models.Transaction, {
+        foreignKey: 'user_id'
+      })
+      userModel.hasMany(models.Order, {
+        foreignKey: 'user_id'
+      })
+      
+    }
+    return userModel
 }
 
 module.exports = userModel
