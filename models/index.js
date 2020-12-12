b6c13de2e6cec0aa424418883ce5c71cdc806278
Binni Zenobio Cordova Leandro <binni.2000.cordova@gmail.com>
2020-12-11 21:58:51 -0500

    filter on Items query

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const CredentialModel = require('./credentialModel')
 const CardModel = require('./cardModel')
 const ItemModel = require('./itemModel')
 
-const sequelize = new Sequelize('mysql://root:root@localhost:3306/apachimuhkayqui')
+const sequelize = new Sequelize('mysql://bcordova:Platanit05@primate-migrate-ultimate-cluster.cluster-cqscyymggatg.us-east-1.rds.amazonaws.com:3306/apachimuhkayqui')
 
 const User = UserModel(sequelize, Sequelize)
 const Credential = CredentialModel(sequelize, Sequelize)
